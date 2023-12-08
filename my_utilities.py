86f1cbe2ac29554be4c4d5d2ef7894a3e5945050
taliah-hl <taliah-hl-1111@protonmail.com>
2023-12-08 13:15:39 +0800

    update

--- my_utilities.py
+++ my_utilities.py
@@ -1,6 +1,9 @@
 import json
 import os
 import sys
+import pandas as pd
+from tabulate import tabulate
+
 def dict_to_txt(dictToPrint: dict,  save_path:str=None, file_prefix: str='', textbox: str=None):
         if save_path is None:
             save_path='./result/'
@@ -8,7 +11,9 @@ def dict_to_txt(dictToPrint: dict,  save_path:str=None, file_prefix: str='', tex
             os.makedirs(save_path)
 
         add_num =1
-        save_path += f"/train_1_medic_accuracy"
+        if len(file_prefix) ==0:
+             file_prefix = "accuracy"
+        save_path += f"/{file_prefix}"
         save_path_norepeat = save_path
         while os.path.isfile(f'{save_path_norepeat}.txt'):
             save_path_norepeat = save_path + f'_{add_num}'
@@ -32,3 +37,37 @@ def dict_to_txt(dictToPrint: dict,  save_path:str=None, file_prefix: str='', tex
                     file.write('\n')
             except:
                 pass
+        print(f"{file_prefix} saved to {save_path_norepeat}")
+
+def df_to_csv(df: pd.DataFrame, save_path:str=None, file_prefix: str='', textbox: str=None):
+        """
+        export df to csv in dir {save_path} with auto file naming
+        """
+        if save_path is None:
+            save_path='./result/'
+        if not os.path.isdir(save_path):
+            os.makedirs(save_path)
+
+        add_num =1
+        if len(file_prefix) ==0:
+             file_prefix = "predictions"
+        save_path += f"/{file_prefix}"
+        save_path_norepeat = save_path
+        while os.path.isfile(f'{save_path_norepeat}.csv'):
+            save_path_norepeat = save_path + f'_{add_num}'
+            add_num +=1
+
+        save_path_norepeat += '.csv'
+        df.to_csv(save_path_norepeat)
+        if textbox is not None:
+            with open(save_path_norepeat, 'a', encoding='utf-8') as fio:
+                fio.write(textbox)
+                fio.write('\n')
+
+        print(f"{file_prefix} saved to {save_path_norepeat}")
+
+
+def print_df( df, title:str=None):
+    if title is not None:
+        print(title)
+    print("\n"+tabulate(df, headers='keys', tablefmt='psql', floatfmt=(".0f")))
--- readme.md
+++ readme.md
@@ -14,8 +14,21 @@ conda install --yes --file requirements.txt
 ## train one medicine each time
 `cnn_simplified_data2.ipynb`
 
-## train all medicine at one time (未完成)
-`cnn_simplified_data2 mulyi_classifer.ipynb`
+## train all medicine at one time 
+`cnn_simplified_data2 multi_classifer.ipynb`
+
+### Cheat sheet
+
+| var | description|
+|---|---|
+| `df_predictions`  | probability of "has" that medicine (有該藥材)  |
+| `all_class_binary_prediction_df`  | predicted as 是否有該藥材  |
+|  `val_y` | ground truth of 是否有該藥材  |
+|   |   |
+
+`df_predictions` =  probability of "has" that medicine (有該藥材)
+`all_class_binary_prediction_df` = predicted as 是否有該藥材
+`val_y` = ground truth of 是否有該藥材
 .
 
 .
@@ -33,64 +46,6 @@ conda install --yes --file requirements.txt
 
 `try_nn_1.ipynb`
 
-## possible steps (???)
-
-抓我們資料裡其中一小部分來試驗
-or
-模仿我們data 的模式generate一分假的資料出來做試驗
-
-# Steps of CNN
-
-### 文本處理
-
-    文本向量化：將文本數據轉換為數值表示形式，比如將中文文字轉換成詞向量或字符向量。
-    填充序列：確保所有文本序列的長度相同，如果需要，進行填充或截斷操作。
-
-### 建立CNN模型
-
-    卷積層與池化層：設計卷積層和池化層來提取特徵並減少輸入的維度。
-    Flatten層與全連接層：將卷積池化後的特徵展平成向量，然後添加全連接層進行分類。
-
-### 模型訓練與評估
-
-    編譯模型：選擇損失函數、優化器和評估指標。
-    訓練模型：使用已處理的數據訓練CNN模型，通常通過反向傳播算法來更新權重。
-    評估模型：使用測試集來評估模型的性能，看準確度或其他指標。
-
-### 模型調整和優化
-
-    超參數調整：調整卷積層的數量、大小，池化層的選擇等，以提高模型性能。
-    防止過擬合：使用正則化方法，如dropout層，以防止模型在訓練集上表現好但在測試集上表現差。
-
-## CNN概念圖
-
-![Alt text](CNN_explain.jpg)
-
-## 3 Layers of CNN
-
-### Convolution Layer卷積層
-
-- 把matrix 相乘後再相加
-` input map X feature detector --->  feature map `
-
-### Pooling Layer 池化層
-- one of the common pooling method: max pooling
-- cut big matrix into submatrices, find the max of submatrices
-
-```
-            max pooling
- feature map ------->  pooled feature map 
-```
-
-### Fully Connected Layer 全連接層
-- 把pooled feature map flatten成column vector (最基本的神經網絡)
-
-```
-            flattening
- pooled feature map ------->  a basic CNN 
-```
-
-![Alt text](CNN_steps_explain.jpg)
 
 ## 例子
 
